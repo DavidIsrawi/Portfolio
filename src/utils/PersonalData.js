fa789531a5259f8651539bb4adb2768eab20e1b9
David <davidisrawi@gmail.com>
2021-12-07 05:26:49 +0000

    mocked jsons for info

---
+++ src/utils/PersonalData.js
@@ -0,0 +1,149 @@
+export const personalData = [
+    {
+        "_id": "61aed62004f6c5437e2dc855",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Inrt"
+        },
+        "description": {
+            "title": "Centuria",
+            "description": "Esse veniam cillum anim aute qui duis sint ad sit nisi adipisicing enim. Sint sunt amet ad sit voluptate velit aute. Cillum veniam reprehenderit id ullamco et voluptate nostrud nisi id amet enim. Voluptate irure aliquip pariatur non anim enim dolor nisi ad cillum cupidatat laboris. Consequat magna nostrud elit ut voluptate fugiat quis adipisicing. Nostrud esse ad reprehenderit tempor ipsum amet occaecat et in fugiat aliqua cupidatat.\r\n"
+        },
+        "links": [
+            {
+                "title": "Conner",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Tyson",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Poole",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed620e0971915f633a2aa",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Firewax"
+        },
+        "description": {
+            "title": "Uni",
+            "description": "Laboris ex laborum ullamco veniam. Magna exercitation labore officia et dolor sunt sunt adipisicing id. Est esse ipsum dolor ut anim sit occaecat tempor ea est occaecat. Dolore dolore aliquip adipisicing aliqua ipsum commodo deserunt velit reprehenderit officia. Irure dolor nulla est ex dolor nostrud.\r\n"
+        },
+        "links": [
+            {
+                "title": "Lindsay",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Park",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed620e71ad3d17c061cd7",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Baluba"
+        },
+        "description": {
+            "title": "Enersol",
+            "description": "Incididunt aliqua laborum veniam ullamco do do ipsum do. Proident mollit et in occaecat voluptate Lorem est dolore. Ut aliqua laboris reprehenderit duis sint in et sit enim. Velit occaecat culpa aliqua incididunt ipsum in aliquip. Fugiat nisi culpa ipsum nulla consectetur irure enim anim Lorem laboris ullamco consequat labore ea.\r\n"
+        },
+        "links": [
+            {
+                "title": "Sellers",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed62057523400f9b0274f",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Pearlesex"
+        },
+        "description": {
+            "title": "Maxemia",
+            "description": "Nisi sint in nulla dolor ex exercitation eiusmod veniam. In mollit culpa reprehenderit irure fugiat qui deserunt nisi labore nostrud eiusmod in fugiat. Et adipisicing minim laboris tempor dolor anim id fugiat ut anim. Dolor veniam labore qui deserunt eiusmod. Irure nisi sint duis sit culpa tempor aute. Sint eiusmod laborum veniam sint qui ullamco et excepteur dolore nostrud voluptate qui aliquip. Fugiat eiusmod magna minim cillum magna deserunt veniam.\r\n"
+        },
+        "links": [
+            {
+                "title": "Richards",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Shannon",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Wood",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed620dd166555613cfbce",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Fangold"
+        },
+        "description": {
+            "title": "Techtrix",
+            "description": "Proident magna eu proident ex aliqua veniam consectetur velit mollit ullamco magna dolor exercitation. Consequat ullamco duis consectetur est reprehenderit. Culpa enim duis laboris Lorem enim cillum voluptate officia qui et voluptate. Do velit occaecat qui excepteur esse ipsum sit nisi eiusmod ex sint sunt.\r\n"
+        },
+        "links": [
+            {
+                "title": "Stafford",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Mckenzie",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Lawrence",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed620e12a2335aa1f3ebf",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Insectus"
+        },
+        "description": {
+            "title": "Qualitern",
+            "description": "Laborum occaecat laborum reprehenderit Lorem amet cillum eu non. Ea culpa pariatur ut est ad amet elit laborum ex ad aliquip duis esse ipsum. Nisi dolor aliqua sunt qui minim ad voluptate fugiat aliquip Lorem laborum. Proident pariatur sit Lorem commodo sit. Aute consectetur adipisicing nulla laboris est. Ut esse ullamco occaecat sint veniam veniam veniam culpa quis quis labore. Duis reprehenderit sit exercitation cillum pariatur est enim exercitation deserunt fugiat occaecat sunt.\r\n"
+        },
+        "links": [
+            {
+                "title": "Justice",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed6209f87c85a2c2a32b9",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Marqet"
+        },
+        "description": {
+            "title": "Sybixtex",
+            "description": "Dolore non ex nostrud occaecat exercitation Lorem est nostrud occaecat reprehenderit voluptate ipsum. Mollit consectetur culpa eu consectetur culpa anim magna veniam deserunt cupidatat commodo nulla consectetur. Minim adipisicing enim pariatur exercitation laborum esse exercitation enim est reprehenderit minim magna nostrud. Id fugiat elit id pariatur do culpa sit quis nisi laborum.\r\n"
+        },
+        "links": [
+            {
+                "title": "Randall",
+                "url": "www.google.com"
+            }
+        ]
+    }
+]
---
+++ src/utils/ProfessionalData.js
@@ -0,0 +1,111 @@
+export const professionalData = [
+    {
+        "_id": "61aed5a7bd0f5c0c1a11508f",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Xumonk"
+        },
+        "description": {
+            "title": "Cubicide",
+            "description": "Fugiat laboris ut quis magna enim et anim. Dolor aliqua Lorem incididunt dolore officia exercitation voluptate ad tempor. Elit ut sit sunt do dolor ex cupidatat deserunt mollit exercitation fugiat reprehenderit. Amet sunt id anim tempor pariatur cillum irure excepteur voluptate tempor irure excepteur cillum non. Officia nostrud do officia duis consequat cillum aute exercitation. Non consectetur nulla consequat eu officia cillum.\r\n"
+        },
+        "links": [
+            {
+                "title": "Sargent",
+                "url": "www.google.com"
+            },
+            {
+                "title": "England",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed5a7620aae71f144f682",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Pigzart"
+        },
+        "description": {
+            "title": "Genesynk",
+            "description": "Laboris nostrud veniam nostrud nulla elit amet aute nisi incididunt. Dolore esse aliqua ad ut id occaecat. Et sunt sit consequat ea esse veniam excepteur magna elit sit Lorem. Reprehenderit mollit tempor do consectetur aute ea aliqua cillum in sint laborum do amet. Cupidatat amet amet sit consequat eiusmod irure mollit. Fugiat in cillum ex minim. Irure do enim reprehenderit reprehenderit fugiat.\r\n"
+        },
+        "links": [
+            {
+                "title": "Faulkner",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Foreman",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Edwards",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed5a7b53f97c6623ca90a",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Fuelworks"
+        },
+        "description": {
+            "title": "Collaire",
+            "description": "Exercitation ea magna ad do. Quis ex ea exercitation velit laboris duis. Laborum dolore ipsum adipisicing sit est excepteur ex in. Pariatur non proident sit deserunt incididunt eu cupidatat minim minim consequat sunt nulla et.\r\n"
+        },
+        "links": [
+            {
+                "title": "Kirby",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Roth",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed5a75f27463f601bc2a3",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Gogol"
+        },
+        "description": {
+            "title": "Zillidium",
+            "description": "Dolor cupidatat mollit eiusmod ipsum proident eu exercitation ut incididunt exercitation. Velit laborum magna aute dolore non eiusmod cupidatat laborum laboris aute irure. Ut aute cillum ipsum cillum deserunt amet excepteur tempor laborum. Ad dolore aliquip consectetur aute magna excepteur excepteur. Nostrud esse ad sint laborum ex dolore ut eu exercitation laborum eiusmod labore do. In nostrud sit esse dolor anim ipsum.\r\n"
+        },
+        "links": [
+            {
+                "title": "Brennan",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Skinner",
+                "url": "www.google.com"
+            }
+        ]
+    },
+    {
+        "_id": "61aed5a7d5442d31f1b70807",
+        "img": {
+            "path": "http://placehold.it/32x32",
+            "title": "Uncorp"
+        },
+        "description": {
+            "title": "Conjurica",
+            "description": "Incididunt id sunt occaecat sint sint ea. Enim ad nulla minim aliqua culpa. Proident aute eu sint quis.\r\n"
+        },
+        "links": [
+            {
+                "title": "Albert",
+                "url": "www.google.com"
+            },
+            {
+                "title": "Luna",
+                "url": "www.google.com"
+            }
+        ]
+    }
+]
