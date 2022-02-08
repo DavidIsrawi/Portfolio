703960a9fe0ac064d6c6b33fd7bfb7b88fe1f620
David <davidisrawi@gmail.com>
2022-02-07 21:15:48 -0800

    smoother transition animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       },
       fontFamily: {
         'main': ['Montserrat', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+      },
+      transitionDuration: {
+        '400': '400ms'
       }
     },
   },
