1c7a9ba5f8db314571fbf477135939f4c4b36332
David <davidisrawi@gmail.com>
2022-02-22 17:41:50 -0800

    turning sections to regions for accessibility

--- src/Content.tsx
+++ src/Content.tsx
@@ -26,14 +26,14 @@ interface ContentSectionTitleProps {
 const ContentSectionTitle = (props: ContentSectionTitleProps) => {
     return (
         <h2 className="mx-auto w-2/3 flex items-center gap-6">
-            <strong className="mx-auto md:mx-0" tabIndex={0}>{props.title}</strong>
+            <strong className="mx-auto md:mx-0" tabIndex={0} id={props.title}>{props.title}</strong>
         </h2>
     )
 }
 
 const ContentSection = (props: ContentSectionProps) => {
     return (
-        <section id={props.section.type} className="my-10 md:scroll-mt-14 scroll-mt-20">
+        <section className="my-10 md:scroll-mt-14 scroll-mt-20" aria-labelledby={props.section.type}>
             <ContentSectionTitle title={props.section.type}/>
             {
                 props.section.content.map((item: ContentItem) => {
